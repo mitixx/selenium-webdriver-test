37ce8ceb346286fe178ec2c40761aef39cff82fc
未知 <haruokame05@yahoo.co.jp>
2017-08-31 22:34:34 +0900

    test.cs
    selenium-webdriver C#.ver

---
+++ test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace selenium_webdriver_test_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //chrome driverの呼び出し
+            IWebDriver chrome = new ChromeDriver();
+            SearchGoogle(chrome);
+
+
+        }
+        //処理内容
+        static void SearchGoogle(IWebDriver _webdriver)
+        {
+            _webdriver.Url = "https://www.google.co.jp";
+            IWebElement element = _webdriver.FindElement(By.Name("q"));
+            element.SendKeys("Hello WebDriver!!C#!!");
+            element.Submit();
+            while(true){
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+            }
+            //webDriver.Quit();
+        }
+    }
+}
